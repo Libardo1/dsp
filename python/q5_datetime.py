026dd4339aa846287114e0efc783666ee83f5915
emilyinamillion <emilymardelbarry@gmail.com>
2016-03-14 16:17:49 -0700

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,20 +1,36 @@
-# Hint:  use Google to find python function
+import datetime as dt
+import string
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+def number_days(day1, day2):
+	da_stop = (day1.replace('-', '').replace('Jan', '1').replace('Feb', '2').replace('Mar', '3').replace('Apr', '4').replace('May', '5').replace('Jun', '6').replace('Jul', '7').replace('Aug', '8').replace('Sep', '9').replace('Oct', '10').replace('Nov', '11').replace('Dec', '12'))
+	da_stop = int(da_stop)
+	da_stop = dt.date(da_stop)
+    
+	da_start = (day2.replace('-', '').replace('Jan', '1').replace('Feb', '2').replace('Mar', '3').replace('Apr', '4').replace('May', '5').replace('Jun', '6').replace('Jul', '7').replace('Aug', '8').replace('Sep', '9').replace('Oct', '10').replace('Nov', '11').replace('Dec', '12'))
+	da_start = int(da_start)
+	da_start = dt.date(da_start)
+    
+	diff = (da_stop-da_start).days
+	return(diff)
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+def main():
+	#question a
+	day1 = '01-02-2013'
+	day2 = '07-28-2015'
+	diff = number_days(day1, day2)
+	print('the difference between', day1, 'and', day2, 'is', str(diff), 'days')
+	
+	#question b
+	day1 = '12312013'
+	day2 = '05282015'
+	diff = number_days(day1, day2)
+	print('the difference between', day1, 'and', day2, 'is', str(diff), 'days')
+	
+	#question c
+	day1 = '15-Jan-1994'
+	day2 = '14-Jul-2015'
+	diff = number_days(day1, day2)
+	print('the difference between', day1, 'and', day2, 'is', str(diff), 'days')
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015' 
-
-import datetime
-def number_days():
-    date_stop = datetime.datetime(2015, 7, 28)
-    date_start = datetime.datetime(2013, 1, 2)
-    print((date_stop-date_start).days)
-number_days()
+if __name__ == '__main__':
+	main()
